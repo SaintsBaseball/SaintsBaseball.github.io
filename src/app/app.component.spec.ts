9f2ad52b411839b2ab42648c7fbc00d6d474c792
HUNTER\Eli <elimaga1996@gmail.com>
2020-03-04 10:23:35 -0800

    t - added test that navigation bar component is on the app root page

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,10 +1,12 @@
 import {TestBed, async, ComponentFixture} from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
+import { NavigationBarComponent } from "./navigation-bar/navigation-bar.component";
 
 describe('AppComponent', () => {
   let fixture: ComponentFixture<AppComponent>;
   let appComponent: AppComponent;
+  let nativeElement;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -12,15 +14,23 @@ describe('AppComponent', () => {
         RouterTestingModule
       ],
       declarations: [
-        AppComponent
+        AppComponent,
+        NavigationBarComponent
       ],
     }).compileComponents();
 
     fixture = TestBed.createComponent(AppComponent);
     appComponent = fixture.componentInstance;
+    fixture.detectChanges();
+    nativeElement = fixture.nativeElement;
   }));
 
   it('should create the app', () => {
     expect(appComponent).toBeTruthy();
+  });
+
+
+  it('should render the navigation bar', () => {
+    expect(nativeElement.querySelector('navigation-bar')).toBeTruthy();
   });
 });
