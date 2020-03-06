920d5e4dac701e1d58ade51adcd6d2355ebfc25a
HUNTER\Eli <elimaga1996@gmail.com>
2020-03-06 08:41:23 -0800

    R - forgot to remove header component from the app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,14 +5,12 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavigationBarComponent } from './navigation-bar/navigation-bar.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
-import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavigationBarComponent,
-    SidebarComponent,
-    HeaderComponent
+    SidebarComponent
   ],
   imports: [
     BrowserModule,
