167b0d6d037ded0a0960b23914d761afce274546
Eli Magallanes <elimaga1996@gmail.com>
2020-03-19 08:02:25 -0700

    F - Added a constructor for the social media account info class

--- src/app/social-media-account-info.spec.ts
+++ src/app/social-media-account-info.spec.ts
@@ -1,7 +1,17 @@
 import { SocialMediaAccountInfo } from './social-media-account-info';
 
 describe('SocialMediaAccountInfo', () => {
-  it('should create an instance', () => {
-    expect(new SocialMediaAccountInfo()).toBeTruthy();
+  it('should create the instance of the class', () => {
+    const linkToAccount = 'http://wikipedia.com';
+    const linkTitle = 'title';
+    const imageSource = '../../image/src';
+    const imageAlternate = 'Alternate Image';
+
+    const socialMediaAccountInfo = new SocialMediaAccountInfo(linkToAccount, linkTitle, imageSource, imageAlternate);
+
+    expect(socialMediaAccountInfo.linkToAccount).toBe(linkToAccount);
+    expect(socialMediaAccountInfo.linkTitle).toBe(linkTitle);
+    expect(socialMediaAccountInfo.imageSource).toBe(imageSource);
+    expect(socialMediaAccountInfo.imageAlternate).toBe(imageAlternate);
   });
 });
--- src/app/social-media-account-info.ts
+++ src/app/social-media-account-info.ts
@@ -3,4 +3,11 @@ export class SocialMediaAccountInfo {
     linkTitle: string;
     imageSource: string;
     imageAlternate: string;
+
+    constructor(linkToAccount: string, linkTitle: string, imageSource: string, imageAlternate: string) {
+        this.linkToAccount = linkToAccount;
+        this.linkTitle = linkTitle;
+        this.imageSource = imageSource;
+        this.imageAlternate = imageAlternate;
+    }
 }
