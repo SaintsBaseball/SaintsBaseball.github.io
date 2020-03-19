03837ef8ae8acc9888c74c8503cd9fa329b2121b
Eli Magallanes <elimaga1996@gmail.com>
2020-03-19 07:53:17 -0700

    g - Generated a class for the social media account info

---
+++ src/app/social-media-account-info.spec.ts
@@ -0,0 +1,7 @@
+import { SocialMediaAccountInfo } from './social-media-account-info';
+
+describe('SocialMediaAccountInfo', () => {
+  it('should create an instance', () => {
+    expect(new SocialMediaAccountInfo()).toBeTruthy();
+  });
+});
