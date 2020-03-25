181dfc8034253d741ba6d4ed5eebda8a2e438463
Eli Magallanes <elimaga1996@gmail.com>
2020-03-25 07:48:20 -0700

    F - generated a batting statistics class and statistics interface

---
+++ src/app/classes/batting-statistics.spec.ts
@@ -0,0 +1,7 @@
+import { BattingStatistics } from './batting-statistics';
+
+describe('BattingStatistics', () => {
+  it('should create an instance', () => {
+    expect(new BattingStatistics()).toBeTruthy();
+  });
+});
---
+++ src/app/classes/batting-statistics.ts
@@ -0,0 +1,4 @@
+import { IStatistics } from '../interfaces/i-statistics';
+
+export class BattingStatistics implements IStatistics {
+}
