10e66be4b425bc80e4027a82867969f99447e1fb
Eli Magallanes <elimaga1996@gmail.com>
2020-03-26 07:15:47 -0700

    F - Created the instance variables for HittingStatistics

--- src/app/classes/hitting-statistics.ts
+++ src/app/classes/hitting-statistics.ts
@@ -1,4 +1,31 @@
 import { IStatistics } from '../interfaces/i-statistics';
 
 export class HittingStatistics implements IStatistics {
+    private G: number;
+    private AB: number;
+    private R: number;
+    private H: number;
+    private '2B': number;
+    private '3B': number;
+    private HR: number;
+    private RBI: number;
+    private BB: number;
+    private SO: number;
+    private SB: number;
+    private CS: number;
+    private AVG: string;
+    private OBP: string;
+    private SLG: string;
+    private OPS: string;
+    private IBB: number;
+    private HBP: number;
+    private SAC: number;
+    private SF: number;
+    private TB: number;
+    private XBH: number;
+    private GDP: number;
+    private GO: number;
+    private AO: number;
+    private GO_AO: string;
+    private PA: number;
 }
