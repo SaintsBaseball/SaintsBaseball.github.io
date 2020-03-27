e7dfdd5d653c7137fedaa3986f899b1b12f3b6fb
Eli Magallanes <elimaga1996@gmail.com>
2020-03-27 08:03:50 -0700

    t - Have a failing test for creating the database

--- src/app/in-memory-stats-data.service.spec.ts
+++ src/app/in-memory-stats-data.service.spec.ts
@@ -1,6 +1,7 @@
 import { TestBed } from '@angular/core/testing';
 
 import { InMemoryStatsDataService } from './in-memory-stats-data.service';
+import { PlayerHittingStatistics } from './classes/player-hitting-statistics';
 
 describe('InMemoryDataServiceService', () => {
   let inMemoryStatsDataService: InMemoryStatsDataService;
@@ -16,7 +17,11 @@ describe('InMemoryDataServiceService', () => {
 
   describe('createDb', () => {
     it('should create the database with a list of player hitting statistics', () => {
+      const actualDatabase = inMemoryStatsDataService.createDb();
 
+      expect(typeof actualDatabase).toBe('object');
+      expect(typeof actualDatabase['Fall 2019-2020'].length).toBeTruthy();
+      expect(actualDatabase['Fall 2019-2020'][0]).toBeInstanceOf(PlayerHittingStatistics);
     });
   });
 });
--- src/app/in-memory-stats-data.service.ts
+++ src/app/in-memory-stats-data.service.ts
@@ -1,9 +1,17 @@
 import { Injectable } from '@angular/core';
+import { InMemoryDbService } from 'angular-in-memory-web-api';
+import * as stats from '../../filebase/statisticsFileBase';
 
 @Injectable({
   providedIn: 'root'
 })
-export class InMemoryStatsDataService {
+export class InMemoryStatsDataService implements InMemoryDbService {
 
   constructor() { }
+
+  createDb() {
+    return {
+      'Fall 2019-2020': []
+    };
+  }
 }
