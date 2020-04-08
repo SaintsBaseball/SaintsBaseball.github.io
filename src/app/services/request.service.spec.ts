bf71d90985df929b2c6fe93d150d94c7f2c5235d
Eli Magallanes <elimaga1996@gmail.com>
2020-04-08 08:11:18 -0700

    F - WIP writing the code and test for get requests

--- src/app/services/request.service.spec.ts
+++ src/app/services/request.service.spec.ts
@@ -1,16 +1,37 @@
 import { TestBed } from '@angular/core/testing';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 
 import { RequestService } from './request.service';
 
 describe('RequestService', () => {
-  let service: RequestService;
+  let requestService: RequestService;
+  let httpMock: HttpTestingController;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(RequestService);
+    TestBed.configureTestingModule({      
+      imports: [HttpClientTestingModule],
+      providers: [RequestService]
+    });
+    requestService = TestBed.get(RequestService);
+    httpMock = TestBed.get(HttpTestingController);
+  });
+
+  afterEach(() => {
+    httpMock.verify();
   });
 
   it('should be created', () => {
-    expect(service).toBeTruthy();
+    expect(requestService).toBeTruthy();
+  });
+
+  describe('get', () => {
+    it('should make an http request to get information', () => {
+      const randomUrl = 'https://blahUrl.com/some/random/route';
+
+      requestService.get(randomUrl);
+
+      const req = httpMock.expectOne(randomUrl);
+      expect(req.request.method).toBe('GET');
+    });
   });
 });
--- src/app/services/request.service.ts
+++ src/app/services/request.service.ts
@@ -1,10 +1,15 @@
 import { Injectable } from '@angular/core';
 import { IRequestService } from '../interfaces/i-request-service';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class RequestService implements IRequestService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  get(url: string) {
+    this.http.get<any>(url);
+  }
 }
