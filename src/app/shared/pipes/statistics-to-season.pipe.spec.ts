1ecb48ff9f28cfa0361fd1e91f4d63ce47c51ac8
Eli Magallanes <elimaga1996@gmail.com>
2020-07-17 07:34:45 -0700

    F - created a pipe for getting the keys off of the database table

---
+++ src/app/shared/pipes/statistics-to-season.pipe.spec.ts
@@ -0,0 +1,34 @@
+import { StatisticsToSeasonPipe } from './statistics-to-season.pipe';
+import { StatisticsDatabaseTable } from 'src/app/in-memory-data-service/statistics-database-table';
+
+fdescribe('StatisticsToSeasonPipe', () => {
+  let statisticsToSeasonPipe: StatisticsToSeasonPipe;
+
+  beforeEach(() => {
+    statisticsToSeasonPipe = new StatisticsToSeasonPipe();
+  });
+
+  it('create an instance', () => {
+    expect(statisticsToSeasonPipe).toBeTruthy();
+  });
+
+  describe('transform', () => {
+    it('should transform an empty table to an empty string array', () => {
+      const statistics = new StatisticsDatabaseTable();
+
+      const result = statisticsToSeasonPipe.transform(statistics);
+
+      expect(result).toEqual([]);
+    });
+
+    it('should transform a populated table to a string array with the season names', () => {
+      const statistics = new StatisticsDatabaseTable();
+      statistics["Fall 2019-2020"] = [];
+      statistics["Spring 2019"] = [];
+
+      const result = statisticsToSeasonPipe.transform(statistics);
+
+      expect(result).toEqual(["Fall 2019-2020", "Spring 2019"]);
+    });
+  });
+});
---
+++ src/app/shared/pipes/statistics-to-season.pipe.ts
@@ -0,0 +1,13 @@
+import { Pipe, PipeTransform } from '@angular/core';
+import { StatisticsDatabaseTable } from 'src/app/in-memory-data-service/statistics-database-table';
+
+@Pipe({
+  name: 'statisticsToSeason'
+})
+export class StatisticsToSeasonPipe implements PipeTransform {
+
+  transform(value: StatisticsDatabaseTable): string[] {
+    return Object.keys(value);
+  }
+
+}
