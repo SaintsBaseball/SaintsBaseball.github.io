6865cc9f8ddd21bae8ae51c1287418587efc5baa
Eli Magallanes <elimaga1996@gmail.com>
2020-10-12 07:58:39 -0700

    t - fixing url segment not found error

--- src/app/home/sidebar/sidebar.component.spec.ts
+++ src/app/home/sidebar/sidebar.component.spec.ts
@@ -1,6 +1,7 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { SidebarComponent } from './sidebar.component';
+import {WelcomeComponent} from '../welcome/welcome.component';
 
 describe('SidebarComponent', () => {
   let sidebarComponent: SidebarComponent;
@@ -13,7 +14,9 @@ describe('SidebarComponent', () => {
         SidebarComponent
       ],
       imports: [
-        RouterTestingModule
+        RouterTestingModule.withRoutes(
+          [{path: 'statistics', component: WelcomeComponent}]
+        )
       ]
     })
     .compileComponents();
