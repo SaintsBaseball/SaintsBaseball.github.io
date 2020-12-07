dae851c9bc300565527a9dd7cea02f80c390cc73
Eli Magallanes <elimaga1996@gmail.com>
2020-12-07 11:13:44 -0800

    B!! - pulled the routes out of the children

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,24 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { WelcomeComponent } from './home/welcome/welcome.component';
-import { AppComponent } from './app.component';
 
 const routes: Routes = [
   {
     path: '',
-    component: AppComponent,
-    children: [
-      { path: 'welcome', component: WelcomeComponent },
-      {
-        path: 'statistics',
-        loadChildren: () => import('./statistics/statistics.module').then(m => m.StatisticsModule)
-      },
-      {
-        path: 'players',
-        loadChildren: () => import('./players/players.module').then(m => m.PlayersModule)
-      },
-      { path: '', redirectTo: '/welcome', pathMatch: 'full' }
-    ]
+    component: WelcomeComponent
+  },
+  {
+    path: 'statistics',
+    loadChildren: () => import('./statistics/statistics.module').then(m => m.StatisticsModule)
+  },
+  {
+    path: 'players',
+    loadChildren: () => import('./players/players.module').then(m => m.PlayersModule)
   },
   { path: '**', redirectTo: '' }
 ];
