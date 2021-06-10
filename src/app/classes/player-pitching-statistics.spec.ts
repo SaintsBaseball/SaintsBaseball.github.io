6b56bb8fe347e995554e4a8133776db6afa4e31d
Eli Magallanes <elimaga1996@gmail.com>
2021-06-10 13:45:28 -0700

    F - added the one season to the player pitching statistics database table

---
+++ src/app/classes/player-pitching-statistics.spec.ts
@@ -0,0 +1,7 @@
+import { PlayerPitchingStatistics } from './player-pitching-statistics';
+
+describe('PlayerPitchingStatistics', () => {
+  it('should create an instance', () => {
+    expect(new PlayerPitchingStatistics()).toBeTruthy();
+  });
+});
--- src/app/in-memory-data-service/player-pitching-statistics-database-table.ts
+++ src/app/in-memory-data-service/player-pitching-statistics-database-table.ts
@@ -1,2 +1,5 @@
+import { PlayerPitchingStatistics } from "../classes/player-pitching-statistics";
+
 export class PlayerPitchingStatisticsDatabaseTable {
+  'Spring 2021'?: PlayerPitchingStatistics[];
 }
