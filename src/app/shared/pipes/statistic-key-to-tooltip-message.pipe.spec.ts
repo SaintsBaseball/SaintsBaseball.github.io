314a9c6894421b6c02cb623e438014857fa0853c
Eli Magallanes <elimaga1996@gmail.com>
2021-06-14 13:48:36 -0700

    F - translate the keys that are shared between hitting and pitching

--- src/app/shared/pipes/statistic-key-to-tooltip-message.pipe.spec.ts
+++ src/app/shared/pipes/statistic-key-to-tooltip-message.pipe.spec.ts
@@ -18,6 +18,17 @@ describe('StatisticKeyToTooltipMessagePipe', () => {
       const result = pipe.transform(invalidKey);
 
       expect(result).toBe('');
+    });
+
+    it('should return the correct tooltip string when a valid key shared between hitting and pitching stats is passed in', () => {
+      const validStatisticKeys = ['G', 'R', 'H', 'HR', 'BB', 'SO', 'CS', 'IBB', 'GO/AO', 'BABIP'];
+      const expectedTooltipMessages = ['Games', 'Runs', 'Hits', 'Home Runs', 'Walks', 'Strikeouts', 'Caught Stealing', 'Intentional Walks', 'Ground Outs Per Fly Out', 'Average on Balls in Play'];
+
+      for (let i = 0; i < validStatisticKeys.length; i++) {
+        const result = pipe.transform(validStatisticKeys[i]);
+
+        expect(result).toBe(expectedTooltipMessages[i]);
+      }
     });  
   });
 });
--- src/app/shared/pipes/statistic-key-to-tooltip-message.pipe.ts
+++ src/app/shared/pipes/statistic-key-to-tooltip-message.pipe.ts
@@ -5,6 +5,19 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class StatisticKeyToTooltipMessagePipe implements PipeTransform {
   transform(statisticKey: string): string {
-    return '';
+    const commonAbbreviationToStatisticDictionary = {
+      G: 'Games',
+      R: 'Runs', 
+      H: 'Hits', 
+      HR: 'Home Runs', 
+      BB: 'Walks', 
+      SO: 'Strikeouts',
+      CS: 'Caught Stealing', 
+      IBB: 'Intentional Walks', 
+      'GO/AO': 'Ground Outs Per Fly Out', 
+      BABIP: 'Average on Balls in Play'
+    };
+
+    return commonAbbreviationToStatisticDictionary[statisticKey] ?? '';
   }
 }
