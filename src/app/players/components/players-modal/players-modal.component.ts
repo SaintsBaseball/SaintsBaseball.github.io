b3d85ba7cb9ce96f3d459736f724288e4836e88f
Eli Magallanes <elimaga1996@gmail.com>
2021-06-16 14:04:31 -0700

    F - generated the players modal

---
+++ src/app/players/components/players-modal/players-modal.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'players-modal',
+  templateUrl: './players-modal.component.html',
+  styleUrls: ['./players-modal.component.css']
+})
+export class PlayersModalComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/players/players.module.ts
+++ src/app/players/players.module.ts
@@ -4,6 +4,7 @@ import { SharedModule } from '../shared/shared.module';
 import { PlayersShellComponent } from './containers/players-shell/players-shell.component';
 import { PlayersListComponent } from './components/players-list/players-list.component';
 import { MaterialModule } from '../material/material.module';
+import { PlayersModalComponent } from './components/players-modal/players-modal.component';
 
 const playersRoutes: Routes = [
   { path: '', component: PlayersShellComponent }
@@ -12,7 +13,8 @@ const playersRoutes: Routes = [
 @NgModule({
   declarations: [
     PlayersShellComponent,
-    PlayersListComponent
+    PlayersListComponent,
+    PlayersModalComponent
   ],
   imports: [
     SharedModule,
